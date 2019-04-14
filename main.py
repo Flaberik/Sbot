d84feb1065fb1990f99d40483617dde0e875be47
DonPadlo <lexa.merkylov99@gmail.com>
2019-04-14 14:31:03 +0300

    +

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def main():
             print('Комманда "' + command + '" не существует')
 #--------------------------------------------------------------
 if __name__ == "__main__":
-    server.run(host="0.0.0.0" port=os.environ.get('PORT', 5000))
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 server = Flask(__name__)
 '''
 if __name__ == "__main__":
